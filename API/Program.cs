5306c1999f2cb86d3194e4748f8e8a7b26bf5836
luishf2014 <luishgamer2237@gmail.com>
2024-07-11 10:48:39 -0300

    criei o react

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,27 @@
 using API.models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Prova Final ");
 
+//Cadastrar, Listar, Consultar, Atualizar e Deletar
+//Usuario
+
 app.MapPost("/api/usuario/cadastrar", ([FromBody] Usuario usuario,
     [FromServices] AppDbContext ctx) =>
 {
     ctx.usuarios.Add(usuario);
     ctx.SaveChanges();
-    return Results.Created($"/produto/{usuario.Id}", usuario);
+    return Results.Created($"/{usuario.Id}", usuario);
 });
+
+
+
+
+
 
 
 
--- API/models/Usuario.cs
+++ API/models/Usuario.cs
@@ -4,5 +4,7 @@ public class Usuario
     public int Id { get; set; }
     public int Cpf { get; set; }
     public string? Nome { get; set; }
+    public double Peso { get; set; }
+    public double Altura { get; set; }
 
 }
